1b08a23b82d2b9c8254bc7b3e8019383912c7a8e
mark swaringen <mswaringen@gmail.com>
2022-11-21 13:01:27 +0000

    updated and working

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ DEBUG = False
 SQLALCHEMY_ECHO = False
 
 # OAuth2 credentials
-CLIENT_ID= 'EnterClientIDHere'
-CLIENT_SECRET = 'EnterClientSecretHere'
+CLIENT_ID= 'AB5TVQBiVK9A9qc7u6Sje6iYy7L4cIToIAJRg1phzoirIkM9L9'
+CLIENT_SECRET = 'QI4TAn87UDmIK5xwYNFFUzafgZZfYO8WjAc2gBFG'
 REDIRECT_URI = 'http://localhost:5000/callback'
 
 # Choose environment; default is sandbox
@@ -11,5 +11,5 @@ ENVIRONMENT = 'Sandbox'
 # ENVIRONMENT = 'Production'
 
 # Set to latest at the time of updating this app, can be be configured to any minor version
-API_MINORVERSION = '23'
+API_MINORVERSION = '65'
 
